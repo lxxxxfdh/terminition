75777164c7c74486857376aa99e01fb548d7d989
xie <qxxxfdh@163.com>
2015-12-10 21:23:22 +0800

    abstract

---
+++ test/generateir.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#myvar='hello world'
+#myvar=$1
+#myvar="${myvar%.*}" 
+#clang -S -emit-llvm "$myvar".c -o ""$myvar".ll"
+#opt -mem2reg -S ""$myvar".ll" -o ""$myvar"reg.ll"
+#rm -f ""$myvar".ll"
+
+function ergodic(){
+
+for file in ` ls $1`
+do
+
+                if [ -d $1"/"$file ] #如果 file存在且是一个目录则为真
+                then
+                      ergodic $1"/"$file $2
+                elif [[ $file == *.c ]] 
+		then
+                      local path=$1"/"$file #得到文件的完整的目录
+                      local name=$file       #得到文件的名字
+			local name1=$2"/"$file".ll1"
+                      local name2=$2"/"$file"reg.ll"
+                      clang -S -emit-llvm $path -o $name1
+		      opt -mem2reg -S $name1 -o $name2
+
+
+                      #做自己的工作.
+               fi
+
+done
+
+
+}
+
+cd $2
+rm -rf *
+ergodic $1 $2
+find $2  -name "*.ll1"|xargs rm -rf
+
